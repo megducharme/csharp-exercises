443bee3d093c3b99314acd6425fca63c18cad96c
Meg Ducharme <ducharme.meg@gmail.com>
2017-07-05 19:39:30 -0500

    initial commit

---
+++ pythonPlanetList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pythonPlanetList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() {"Mercury", "Mars"};
+            //Add() Jupiter and Saturn at the end of the list.
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            //Create another List that contains that last two planet of our solar system.
+            int planetListCount = planetList.Count;
+
+            List<string> lastTwo = planetList.GetRange((planetListCount - 2), 2);
+
+            //Combine the two lists by using AddRange().
+            planetList.AddRange(lastTwo);
+
+            //Use Insert() to add Earth, and Venus in the correct order.
+            planetList.Insert(3, "Earth");
+            planetList.Insert(2, "Venus");
+
+            //Use Add() again to add Pluto to the end of the list.
+            planetList.Add("Pluto");
+
+            //Now that all the planets are in the list, slice the list using GetRange() in order to extract the rocky planets into a new list called rockyPlanets.
+            List<string> rockyPlanets = planetList.GetRange(3, 5);
+            //I dont know which ones are rocky, this is random :)
+
+            //Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList.
+            planetList.Remove("Pluto");
+
+            foreach(string planet in rockyPlanets)
+            {
+                Console.WriteLine(planet);
+            }
+
+        }
+    }
+}
---
+++ transactionTuples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace transactionTuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+
+            transactions.Add(("car", 22000, 3));
+            transactions.Add(("moped", 12000, 34));
+            transactions.Add(("bike", 200, 63));
+
+            double totalSales = 0;
+            int totalItems = 0;
+            foreach ((string product, double amount, int quantity) t in transactions)
+            {
+                totalSales += t.Item2;
+                totalItems += t.Item3;
+            }
+            
+            Console.WriteLine("Items sold today: " + totalItems);
+            Console.WriteLine($"Total revenue: {totalSales:C}");
+        }
+    }
+}
