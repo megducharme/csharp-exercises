1d21d1bf2e02ba1d467bb57d38dc87c16d4283be
Meg Ducharme <ducharme.meg@gmail.com>
2017-07-10 10:24:32 -0500

    added simpler methods exercise

--- simplerMethods/Program.cs
+++ simplerMethods/Program.cs
@@ -1,56 +1,64 @@
-﻿public class Bug
-{
-    /*
-        You can declare a typed public property, make it read-only,
-        and initialize it with a default value all on the same
-        line of code in C#. Readonly properties can be set in the
-        class' constructors, but not by external code.
-    */
-    public string Name { get; } = "";
-    public string Species { get; } = "";
-    public ICollection<string> Predators { get; } = new List<string>();
-    public ICollection<string> Prey { get; } = new List<string>();
+﻿using System;
+using System.Collections.Generic;
 
-    // Convert this readonly property to an expression member
-    public string FormalName
+namespace simplerMethods
+{
+    class Program
     {
-        get
+        static void Main(string[] args)
         {
-            return $"{this.Name} the {this.Species}";
+            Console.WriteLine("Hello World!");
+            List<string> thingsToEat = new List<string>(){"pollen", "stuff", "food"};
+            List<string> thingsThatEatYou = new List<string>(){"cats", "venus fly trap", "frogs"};
+            Bug albert = new Bug("Albert","Bug", thingsToEat, thingsThatEatYou);
+            albert.Eat("Goldfish");
         }
     }
-
-    // Class constructor
-    public Bug(string name, string species, List<string> predators, List<string> prey)
+    public class Bug
     {
-        this.Name = name;
-        this.Species = species;
-        this.Predators.Concat(predators);
-        this.Prey.Concat(prey);
-    }
+        /*
+            You can declare a typed public property, make it read-only,
+            and initialize it with a default value all on the same
+            line of code in C#. Readonly properties can be set in the
+            class' constructors, but not by external code.
+        */
+        public string Name { get; } = "";
+        public string Species { get; } = "";
+        public List<string> Predators { get; } = new List<string>();
+        public List<string> Prey { get; } = new List<string>();
 
-    // Convert this method to an expression member
-    public string PreyList()
-    {
-        var commaDelimitedPrey = string.Join(",", this.Prey);
-        return commaDelimitedPrey;
-    }
+        // Convert this readonly property to an expression member
+        public string FormalName => $"{this.Name} the {this.Species}";
 
-    // Convert this method to an expression member
-    public string PredatorList()
-    {
-        var commaDelimitedPredators = string.Join(",", this.Predators);
-        return commaDelimitedPredators;
-    }
-
-    // Convert this to expression method (hint: use a C# ternary)
-    public string Eat(string food)
-    {
-        if (this.Prey.Contains(food))
+        // Class constructor
+        public Bug(string name, string species, List<string> predators, List<string> prey)
         {
-            return $"{this.Name} ate the {food}.";
-        } else {
-            return $"{this.Name} is still hungry.";
+            this.Name = name;
+            this.Species = species;
+            this.Predators.AddRange(predators);
+            this.Prey.AddRange(prey);
+        }
+
+        // Convert this method to an expression member
+        public string PreyList()
+        {
+            var commaDelimitedPrey = string.Join(",", this.Prey);
+            return commaDelimitedPrey;
+        }
+
+        // Convert this method to an expression member
+        public string PredatorList()
+        {
+            var commaDelimitedPredators = string.Join(",", this.Predators);
+            return commaDelimitedPredators;
+        }
+
+        // Convert this to expression method (hint: use a C# ternary)
+        public string Eat(string food)
+        {
+            string answer = this.Prey.Contains(food) ? $"{this.Name} ate the {food}." : $"{this.Name} is still hungry.";
+            Console.WriteLine(answer);
+            return answer;
         }
     }
 }
