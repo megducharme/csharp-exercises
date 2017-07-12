f03ecaa02a642463d9d3b1d0f811fcc571f00011
Meg Ducharme <ducharme.meg@gmail.com>
2017-07-12 06:56:14 -0500

    added millionaires solution

--- millionaires/Program.cs
+++ millionaires/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace millionaires
 {
@@ -14,31 +15,29 @@ namespace millionaires
     public class Program
     {
         public static void Main() {
-            // List<Customer> customers = new List<Customer>() {
-            //     new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
-            //     new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
-            //     new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
-            //     new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
-            //     new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
-            //     new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
-            //     new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
-            //     new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
-            //     new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
-            //     new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
-            // };
-                
-            // var millionaires = 
-            //     customers.Where(cust => cust.Balance > 1000000)
-            //              .GroupBy(cust.Bank)
-            //              .Select(x => new {key = cust.Key, numberOfCustomers=x.Value});
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
 
-            Console.WriteLine(6 % 1);
-            Console.WriteLine(10 % 2);
-            Console.WriteLine(5 % 2);
-            Console.WriteLine(81 % 9);
-            
-    
+        var millionaires13 = from cust in customers
+                            where(cust.Balance > 1000000)
+                            group cust by cust.Bank into g
+                            select new {Bank = g.Key, Customers = g.ToList()};
+
+        foreach(var person in millionaires13)
+        {
+            Console.WriteLine($"{person.Bank}, {person.Customers.Count}");
         }
-
+            
+        }
     }
 }
